63edc159a82ef688615e470ede0ba54a75d9f092
saifalways <saifkhanmern@gmail.com>
2026-07-24 21:58:17 +0600

    dlt & edit btn

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-let name = document.querySelector(".name");
-let caption = document.querySelector(".caption");
-let btn = document.querySelector(".btn");
-let post = document.querySelector(".post");
+"use strict";
 
-btn.addEventListener("click", () => {
+let name = document.querySelector(".name");
+let caption = document.querySelector(".caption"); 
+let post = document.querySelector(".post");
+let updateBtn = document.querySelector(".updateBtn");
+let postBtn = document.querySelector(".postBtn");
+
+let arr = [];
+
+var indexNum;
+
+
+postBtn.addEventListener("click", () => {
   if (name.value.trim() === "" || caption.value.trim() === "") {
-    post.innerHTML = `<p>Enter name and caption</p>`;
+    post.innerHTML = `Enter a name and caption`;
+    return;
   } else {
     arr.push({
       name: name.value,
@@ -17,18 +26,57 @@ btn.addEventListener("click", () => {
   }
 });
 
-let arr = [];
+updateBtn.addEventListener("click",()=>{ 
 
-function display() {
+      arr[indexNum].name = name.value;
+      arr[indexNum].caption = caption.value;
+
+      postBtn.style.display = "block";
+      updateBtn.style.display = "none"; 
+      display();
+      name.value = "";
+      caption.value = "";
+})
+
+ function display () {
   post.innerHTML = "";
-  arr.map((data) => {
+  arr.forEach((data) => {
     post.innerHTML += `<div class="card m-2" style="width: 16.5rem;">
                 <div class="card-body">
                     <h5 class="card-title">${data.name}</h5>
                     <p class="card-text">${data.caption}</p>
-                    <button href="#" class="btn btn-primary">Edit</button>
-                    <button href="#" class="btn btn-danger">Delete</button>
+                    <button class="btn btn-primary editBtn">Edit</button>
+                    <button class="btn btn-danger dltBtn">Delete</button>
                 </div>
             </div>`;
   });
-}
+
+  let dltBtn = document.querySelectorAll(".dltBtn");                                  //? when we use querySelectorAll it returns us a nodelist.
+
+  // let newname = Array.from(dltBtn);                                                       //! convert node-list to array 
+
+  [...dltBtn].map((item,index) => {                 //?   [...] is spread oparetor which returns us array from nodelist.
+    item.addEventListener("click", () => {
+      console.log(index);
+      arr.splice(index, 1);
+      display();
+      
+    });
+  });
+ 
+  
+  let editBtn = document.querySelectorAll(".editBtn"); 
+  [...editBtn].forEach((eItem, eIndex) => {
+    eItem.addEventListener("click", () => { 
+      
+      indexNum = eIndex;
+      
+      name.value = arr[eIndex].name;
+      caption.value = arr[eIndex].caption;
+      
+      postBtn.style.display = "none";
+      updateBtn.style.display = "block";   
+      dltBtn[eIndex].style.display = "none"; 
+    });
+  }); 
+};
